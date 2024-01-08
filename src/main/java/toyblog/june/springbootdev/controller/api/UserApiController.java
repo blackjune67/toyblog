65e30b623b5ee9ba5332aef5523158a0086bac69
hajune <buea486@gmail.com>
2024-01-08 17:07:50 +0900

    로그아웃 api

--- src/main/java/toyblog/june/springbootdev/controller/api/UserApiController.java
+++ src/main/java/toyblog/june/springbootdev/controller/api/UserApiController.java
@@ -1,7 +1,12 @@
 package toyblog.june.springbootdev.controller.api;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import toyblog.june.springbootdev.dto.AddUserRequest;
 import toyblog.june.springbootdev.service.UserService;
@@ -15,5 +20,11 @@ public class UserApiController {
     public String signup(AddUserRequest addUserRequest) {
         userService.save(addUserRequest);
         return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/logout";
     }
 }
