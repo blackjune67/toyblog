8e73b7e1f9806f5f4109b42724a7d55457220c1a
hajune <buea486@gmail.com>
2024-01-12 00:10:05 +0900

    OAuth2 설정 파일

--- src/main/java/toyblog/june/springbootdev/config/WebSecurityConfig.java
+++ src/main/java/toyblog/june/springbootdev/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -11,12 +12,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import toyblog.june.springbootdev.config.jwt.TokenProvider;
+import toyblog.june.springbootdev.config.oauth.OAuth2AuthorizationRequestBseOnCookieRepository;
+import toyblog.june.springbootdev.config.oauth.OAuth2SuccessHandler;
+import toyblog.june.springbootdev.config.oauth.OAuth2UserCustomService;
+import toyblog.june.springbootdev.repository.RefreshTokenRepository;
 import toyblog.june.springbootdev.service.UserDetailService;
+import toyblog.june.springbootdev.service.UserService;
 
-@Configuration
+/*@Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -51,6 +64,81 @@ public class WebSecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}*/
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserService userService;
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers("/img/**", "/css/**", "/js/**");
+    }
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // * 함수형으로 변경해야할 듯...
+        http.csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/api/token").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll();
+
+        http.oauth2Login()
+                .loginPage("/login")
+                .authorizationEndpoint()
+                .authorizationRequestRepository(oAuth2AuthorizationRequestBseOnCookieRepository())
+                .and()
+                .successHandler(oAuth2SuccessHandler())
+                .userInfoEndpoint()
+                .userService(oAuth2UserCustomService);
+
+        http.logout()
+                .logoutSuccessUrl("/login");
+
+        http.exceptionHandling()
+                .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                        new AntPathRequestMatcher("/api/**"));
+
+        return http.build();
+    }
+
+    @Bean
+    public OAuth2SuccessHandler oAuth2SuccessHandler() {
+        return new OAuth2SuccessHandler(tokenProvider
+                , refreshTokenRepository
+                , oAuth2AuthorizationRequestBseOnCookieRepository()
+                , userService);
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    // * 쿠키에 저장할 인증 요청 및 상태 저장소
+    @Bean
+    public OAuth2AuthorizationRequestBseOnCookieRepository oAuth2AuthorizationRequestBseOnCookieRepository() {
+        return new OAuth2AuthorizationRequestBseOnCookieRepository();
     }
 
     @Bean
