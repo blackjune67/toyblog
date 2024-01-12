1b4db2fd15fb40f5c71665c431d78afe912d0ee4
hajune <buea486@gmail.com>
2024-01-12 15:40:10 +0900

    불필요 주석 제거

--- src/main/java/toyblog/june/springbootdev/config/WebSecurityConfig.java
+++ src/main/java/toyblog/june/springbootdev/config/WebSecurityConfig.java
@@ -5,21 +5,14 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -29,51 +22,7 @@ import toyblog.june.springbootdev.config.oauth.OAuth2AuthorizationRequestBseOnCo
 import toyblog.june.springbootdev.config.oauth.OAuth2SuccessHandler;
 import toyblog.june.springbootdev.config.oauth.OAuth2UserCustomService;
 import toyblog.june.springbootdev.repository.RefreshTokenRepository;
-import toyblog.june.springbootdev.service.UserDetailService;
 import toyblog.june.springbootdev.service.UserService;
-
-/*@Configuration
-@RequiredArgsConstructor
-@EnableWebSecurity
-public class WebSecurityConfig {
-    private final UserDetailService userService;
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console())
-                .requestMatchers("/static/**");
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests((authorize) ->
-                        authorize
-                                .requestMatchers("/login", "/signup", "/user").permitAll()
-                                .anyRequest().authenticated())
-                .formLogin((formLogin) ->
-                        formLogin
-                                .loginPage("/login")
-                                .defaultSuccessUrl("/articles"))
-                .logout((logout) ->
-                        logout
-                                .logoutSuccessUrl("/login")
-                                .invalidateHttpSession(true) // * 세션 삭제
-                ).csrf(AbstractHttpConfigurer::disable) // * 잠시 비활성화
-                .build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}*/
 
 @RequiredArgsConstructor
 @Configuration
