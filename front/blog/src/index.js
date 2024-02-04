b8254be2f9c07656a66855763941e5aa5a3eb438
hajune <buea486@gmail.com>
2024-02-04 23:45:22 +0900

    index js 수정

--- front/blog/src/index.js
+++ front/blog/src/index.js
@@ -2,45 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {DarkOrLight} from "./practice/chapter14/DarkOrLight";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      {/*<DevSupport
-          ComponentPreviews={<Calculator />}
-          // useInitialHook={useInitial}
-      >*/}
-      {/*<CommentList />*/}
-      {/*<NotificationList />*/}
-      {/*  <Calculator />*/}
-      {/*<Accommodate />*/}
-      {/*<ConfirmButton />*/}
-      {/*<ConfirmButtonFn />*/}
-      {/*<AttendanceBook />
-      <LandingPage />*/}
-      {/*<NameForm />*/}
-      {/*<Reservation />*/}
-      {/*<Signup />*/}
-      {/*<Calculator />*/}
-      {/*</DevSupport>*/}
-      {/*<ProfileCard/>*/}
-      <DarkOrLight/>
+      <App/>
     </React.StrictMode>
 );
-/*setInterval(() => {
-  root.render(
-      <React.StrictMode>
-        {/!*<App />*!/}
-        {/!*<Library/>
-        <Clock/>*!/}
-        {/!*<Comment />*!/}
-        <CommentList />
-      </React.StrictMode>
-  );
-}, 1000);*/
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
